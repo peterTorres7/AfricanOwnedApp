ec468835dc3382c825a430bf61a1b88bf8d4ff59
Aaron <imaaronacs@gmail.com>
2021-10-26 19:36:13 -0700

    revert

--- app/src/main/java/com/aaron/cs/africanowned/drawer_fragments/AddListing.java
+++ app/src/main/java/com/aaron/cs/africanowned/drawer_fragments/AddListing.java
@@ -35,39 +35,6 @@ public class AddListing extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_add_listing, container, false);
-        nextbtn = view.findViewById(R.id.next);
-        taglinetext = view.findViewById(R.id.tagline);
-        taglinetext.setVisibility(View.GONE);
-        taglineCheck = view.findViewById(R.id.taglineCheck);
-//On check box select it display the view
-        taglineCheck.setOnClickListener(view -> {
-            if (taglineCheck.isChecked())
-                taglinetext.setVisibility(view.getVisibility());
-
-        });
-        //Create dropdown list for countery
-        dropdown=view.findViewById( R.id.textInputLayout2 );
-        autocomplet=view.findViewById( R.id.countery );
-
-        Locale[] listofCountery = Locale.getAvailableLocales();
-        ArrayList<String> countries = new ArrayList<String>();
-        for (Locale locale : listofCountery) {
-            String country = locale.getDisplayCountry();
-            if (country.trim().length() > 0 && !countries.contains(country)) {
-                countries.add(country);
-            }
-        }
-        Collections.sort(countries);
-        for (String country : countries) {
-            System.out.println(country);
-        }
-
-        ArrayAdapter<String> countryAdapter = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_spinner_item, countries);
-
-        countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the your spinner
-        autocomplet.setAdapter(countryAdapter);
         return v;
     }
 }
