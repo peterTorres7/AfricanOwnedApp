f2bddbc0f40a3fa22a05d91b79d9235a2d787a96
Peter Torres <torres7p@gmail.com>
2021-11-07 15:46:10 -0800

    user sign up info saves to firestore

--- app/src/main/java/com/aaron/cs/africanowned/SignUp.java
+++ app/src/main/java/com/aaron/cs/africanowned/SignUp.java
@@ -15,17 +15,25 @@ import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUp extends AppCompatActivity {
 
-    EditText uName, uEmail, uPassword;
+    EditText uName, uEmail, uPassword, uPhone;
     TextView uLoginButton;
     Button uSignUpButton;
     ProgressBar progressBar;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +43,13 @@ public class SignUp extends AppCompatActivity {
         uName = findViewById(R.id.Name);
         uEmail = findViewById(R.id.email);
         uPassword = findViewById(R.id.password);
+        uPhone = findViewById(R.id.phone);
         uLoginButton = findViewById(R.id.alreadyRegistered);
         uSignUpButton = findViewById(R.id.signUp);
 
         progressBar = findViewById(R.id.progressBar);
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         if(fAuth.getCurrentUser() != null) {
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
@@ -49,11 +59,12 @@ public class SignUp extends AppCompatActivity {
         uSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fullName = uName.getText().toString().trim();
+                String name = uName.getText().toString().trim();
                 String email = uEmail.getText().toString().trim();
                 String password = uPassword.getText().toString().trim();
+                String phone = uPhone.getText().toString();
 
-                if(TextUtils.isEmpty(fullName)) {
+                if(TextUtils.isEmpty(name)) {
                     uName.setError("Name is required.");
                     return;
                 }
@@ -69,6 +80,10 @@ public class SignUp extends AppCompatActivity {
                     uPassword.setError("Password must be at least 7 characters long.");
                     return;
                 }
+                if(TextUtils.isEmpty(phone)) {
+                    uPhone.setError("Phone is required.");
+                    return;
+                }
 
                 progressBar.setVisibility(View.VISIBLE);
 
@@ -77,6 +92,18 @@ public class SignUp extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
                             Toast.makeText(SignUp.this, "User Created", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("name", name);
+                            user.put("email", email);
+                            user.put("phone", phone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(SignUp.this, "User info added", Toast.LENGTH_SHORT).show();
+                                }
+                            });
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         } else {
                             Toast.makeText(SignUp.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
