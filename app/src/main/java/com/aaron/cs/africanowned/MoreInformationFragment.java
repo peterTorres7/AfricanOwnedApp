0d910027c84c5ebbf785487baa71804286c84c2d
Yergalem1 <yergalemt@gmail.com>
2021-11-23 16:34:00 -0500

    Form validation for Bisness_hours Fragment and Faq_Fragment,Social_Media_Fragment

--- app/src/main/java/com/aaron/cs/africanowned/MoreInformationFragment.java
+++ app/src/main/java/com/aaron/cs/africanowned/MoreInformationFragment.java
@@ -3,19 +3,43 @@ package com.aaron.cs.africanowned;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class MoreInformationFragment extends Fragment {
-
+Button nextbtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate( R.layout.fragment_more_information, container, false );
+        View view =inflater.inflate( R.layout.fragment_more_information, container, false );
+        nextbtn = view.findViewById(R.id.next);
+        nextbtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+
+              /*  FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.frame, new SocilMediaFragment());
+                ft.addToBackStack(null);
+                ft.commit();*/
+
+            }
+
+
+
+        });
+
+
+        return view;
     }
+
+
 }
+
