958def3dfeeebc560fc7f8631e838f0c1c2faecf
Aaron <imaaronacs@gmail.com>
2021-11-30 17:35:58 -0800

    details page

--- app/src/main/java/com/aaron/cs/africanowned/RecyclerViewBusinessAdapter.java
+++ app/src/main/java/com/aaron/cs/africanowned/RecyclerViewBusinessAdapter.java
@@ -1,6 +1,7 @@
 package com.aaron.cs.africanowned;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,11 +18,23 @@ public class RecyclerViewBusinessAdapter extends RecyclerView.Adapter<RecyclerVi
     private LayoutInflater inflater;
     private Context context;
     List<String> businesses;
+    static List<String> aboutUs;
+    static List<String> categoryNames;
+    static List<String> hours;
+    static List<String> locationNames;
+    static List<String> websiteNames;
 
-    public RecyclerViewBusinessAdapter(Context context, List<String> businesses) {
+    public RecyclerViewBusinessAdapter(Context context, List<String> businesses, List<String> aboutUs,
+                                       List<String> categoryNames, List<String> hours, List<String> locationNames,
+                                       List<String> websiteNames) {
         inflater = LayoutInflater.from(context);
         this.context = context;
         this.businesses=businesses;
+        this.aboutUs = aboutUs;
+        this.categoryNames = categoryNames;
+        this.hours = hours;
+        this.locationNames = locationNames;
+        this.websiteNames = websiteNames;
     }
 
     @Override
@@ -34,6 +47,22 @@ public class RecyclerViewBusinessAdapter extends RecyclerView.Adapter<RecyclerVi
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
         holder.businessName.setText(businesses.get(position));
+
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), BusinessInformation.class);
+
+                intent.putExtra("BusinessName", businesses.get(position));
+                intent.putExtra("AboutUs", aboutUs.get(position));
+                intent.putExtra("Category", categoryNames.get(position));
+                intent.putExtra("Location", locationNames.get(position));
+                intent.putExtra("Hours", hours.get(position));
+                intent.putExtra("website", websiteNames.get(position));
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -44,9 +73,11 @@ public class RecyclerViewBusinessAdapter extends RecyclerView.Adapter<RecyclerVi
     class MyViewHolder extends RecyclerView.ViewHolder
     {
         TextView businessName;
+        Context context;
 
         public MyViewHolder(View itemView) {
             super(itemView);
+            context = itemView.getContext();
             businessName = (TextView)itemView.findViewById(R.id.businessName);
         }
     }
--- app/src/main/java/com/aaron/cs/africanowned/RecyclerViewHolder.java
+++ app/src/main/java/com/aaron/cs/africanowned/RecyclerViewHolder.java
@@ -1,5 +1,6 @@
 package com.aaron.cs.africanowned;
 
+import android.content.Context;
 import android.view.View;
 import android.widget.TextView;
 
@@ -9,8 +10,11 @@ import androidx.recyclerview.widget.RecyclerView;
 public class RecyclerViewHolder extends RecyclerView.ViewHolder {
 
     private TextView view;
+    private final Context context;
+
     public RecyclerViewHolder(@NonNull View itemView) {
         super(itemView);
+        context = itemView.getContext();
         view = itemView.findViewById(R.id.businessName);
     }
 
